20265d10fdc9cc07bc4dd008be03b040bb96b67c
Adrian Roworth <adrian.roworth@CICA.GOV.UK>
2018-11-27 09:23:58 +0000

    Update redundancies

--- run.bat
+++
@@ -1,2 +0,0 @@
-call docker restart cicav2--data-capture-service
-call docker logs --since 0m -f cicav2--data-capture-service
--- uninstall.bat
+++
@@ -1,5 +0,0 @@
-call docker stop cicav2--data-capture-service
-call docker rm cicav2--data-capture-service
-call docker image rm cicav2-data-capture-service
-
-echo "Data Capture Service uninstalled and no longer running"
